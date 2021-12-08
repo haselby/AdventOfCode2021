0efe931f992de1f7ac48cfcb6d4c90f3c2d4ec61
haselby <jeffrey.haselby@gmail.com>
2021-12-07 17:31:20 -0700

    day1project initial commit

---
+++ 01/day1project/day1project.cpp
@@ -0,0 +1,12 @@
+﻿// day1project.cpp : Defines the entry point for the application.
+//
+
+#include "day1project.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
